e0fe43f59644aa8bd40bca760cf3aab73cba9ce0
rvasa01 <99051473+rvasa01@users.noreply.github.com>
2022-04-06 04:16:42 -0400

    Fully Working Title Screen
    Has tentative Summit logo, working play and world selection buttons.

---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void Switch ()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlayGame ()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    
+    
+
+
+
+   
+
+   
+}
