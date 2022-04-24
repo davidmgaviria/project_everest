a7eca247b0b6a42806147cf089ba818da8a16a96
Hridansh Saraogi <78390395+HridanshS@users.noreply.github.com>
2022-04-24 17:15:59 -0400

    Add files via upload

---
+++ soundManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public static soundManager instance;
+    public AudioSource coinssource;
+    public AudioClip coinSound;
+    public AudioClip deathSound;
+    private void Awake() {
+    	instance = this;
+    }
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+    }
+}
