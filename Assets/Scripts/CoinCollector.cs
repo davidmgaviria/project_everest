727b848b58e8ac5bdce0175addc93c734ef6d1fa
David M. Gaviria <davidmgaviria123@gmail.com>
2022-04-24 23:33:48 -0400

    Integrated sound system

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -9,14 +9,14 @@ public class CoinCollector : MonoBehaviour
     
     private TextMeshProUGUI scoreText;
     private GameObject coinTextObject;
-    //public static AudioClip coinSound;
-    //public AudioClip coinSound;
+    private GameObject musicObject;
 
 
     //when level loads we want score to show the current value
     private void Start() {
         coinTextObject = GameObject.FindGameObjectWithTag("CoinDisplay");
         scoreText = coinTextObject.GetComponent<TextMeshProUGUI>();
+        musicObject = GameObject.FindGameObjectWithTag("SoundManager");
         updateScoreDisplay();
         //coinPickupSound = Music.Load<AudioClip>("Coin_1");
     }
@@ -26,12 +26,8 @@ public class CoinCollector : MonoBehaviour
     	if (other.transform.tag == "coin") {
             PlayerStats.gameScore += 1;
             updateScoreDisplay();
-            //PlaySound(coinPickupSound);
-            //audioSrc.PlaySound("Coin_1")
-            //AudioManager.instance.Play("Coin_1");
-            soundManager.instance.coinssource.PlayOneShot(soundManager.instance.coinSound);
+            musicObject.GetComponent<SoundManager>().playCoinSound();
             Destroy(other.gameObject);
-            //AudioSource.PlayClipAtPoint(coinSound, transform.position);
         }
     }
 
--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -7,19 +7,22 @@ public class PlayerDeath : MonoBehaviour
 {
     
     private GameObject mainCamera;
+    private GameObject musicObject;
 
     private void Start () {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        musicObject = GameObject.FindGameObjectWithTag("SoundManager");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)  {
         if (collision.gameObject.CompareTag("Death") || collision.gameObject.CompareTag("Boulder")) {
-            soundManager.instance.coinssource.PlayOneShot(soundManager.instance.deathSound);
             Die();
         }
     }
 
     private void Die() {
+        musicObject.GetComponent<SoundManager>().playDeathSound();
+
         //update player stats & coin display
         PlayerStats.deathCount++;
         PlayerStats.gameScore += PlayerStats.deathScorePenalty;
